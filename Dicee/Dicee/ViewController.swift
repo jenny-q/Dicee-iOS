49b86f48efbabbab67bb6baeeaa69e13aa2a99c9
Jenny Q <jenny.q@me.com>
2019-05-26 14:27:01 -0400

    initial commit
    Setting up files, images, variables and functions

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by JennyQ on 5/26/19.
+//  Copyright © 2019 Jennifer Quispe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
